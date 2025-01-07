0fe2ccdd95114eec6cf44ac7f12a3d6d7414807a
maiyeecrossley <maiyee.crossley@gmail.com>
2025-01-07 16:36:36 +0000

    practice express API lab

---
+++ data.js
@@ -0,0 +1,75 @@
+export default
+
+[
+    {
+        "id": 1,
+        "name": "Bulbasaur",
+        "type": ["Grass", "Poison"],
+        "pokedex_number": 1,
+        "weakness": ["Fire", "Psychic", "Flying", "Ice"]
+    },
+    {
+        "id": 2,
+        "name": "Ivysaur",
+        "type": ["Grass", "Poison"],
+        "pokedex_number": 2,
+        "weakness": ["Fire", "Psychic", "Flying", "Ice"]
+    },
+    {
+        "id": 3,
+        "name": "Venusaur",
+        "type": ["Grass", "Poison"],
+        "pokedex_number": 3,
+        "weakness": ["Fire", "Psychic", "Flying", "Ice"]
+    },
+    {
+        "id": 4,
+        "name": "Charmander",
+        "type": ["Fire"],
+        "pokedex_number": 4,
+        "weakness": ["Water", "Ground", "Rock"]
+    },
+    {
+        "id": 5,
+        "name": "Charmeleon",
+        "type": ["Fire"],
+        "pokedex_number": 5,
+        "weakness": ["Water", "Ground", "Rock"]
+    },
+    {
+        "id": 6,
+        "name": "Charizard",
+        "type": ["Fire", "Flying"],
+        "pokedex_number": 6,
+        "weakness": ["Water", "Electric", "Rock"]
+    },
+    {
+        "id": 7,
+        "name": "Squirtle",
+        "type": ["Water"],
+        "pokedex_number": 7,
+        "weakness": ["Electric", "Grass"]
+    },
+    {
+        "id": 8,
+        "name": "Wartortle",
+        "type": ["Water"],
+        "pokedex_number": 8,
+        "weakness": ["Electric", "Grass"]
+    },
+    {
+        "id": 9,
+        "name": "Blastoise",
+        "type": ["Water"],
+        "pokedex_number": 9,
+        "weakness": ["Electric", "Grass"]
+    },
+    {
+        "id": 10,
+        "name": "Caterpie",
+        "type": ["Bug"],
+        "pokedex_number": 10,
+        "weakness": ["Fire", "Flying", "Rock"]
+    }
+    
+]
---
+++ practice-server.js
@@ -0,0 +1,59 @@
+import express from "express"
+const app = express()
+
+app.use(express.json())
+import pokemon from "./data.js"
+
+
+app.get("/pokemon", function (req, res) {
+    res.send(pokemon)
+})
+
+app.get("/pokemon/:name", function (req, res) {
+    console.log(req.params.name)
+
+const pokemonName = req.params.name
+
+const pokemons = pokemon.find((currentPokemon) => {
+    return currentPokemon.name.toLowerCase() === pokemonName.toLowerCase()
+})
+
+res.send(pokemons)
+})
+
+
+app.post("/pokemon", function (req, res) {
+    const newPokemon = req.body
+    pokemon.push(newPokemon)
+    res.status(201).send(newPokemon)
+})
+
+
+app.put("/pokemon/:id", function (req, res) {
+    const pokemonId = Number(req.params.id)
+    const updatedPokemon = req.body
+
+    const pokemonIndex = pokemon.findIndex((pokemon) => {
+        return pokemon.id === pokemonId
+    })
+    pokemon[pokemonIndex] = updatedPokemon
+
+    res.send(updatedPokemon)
+})
+
+
+app.delete("/pokemon/:id", function (req, res) {
+    const pokemonId = Number(req.params.id)
+
+    const pokemonIndex = pokemon.findIndex((pokemon) => {
+        return pokemon.id === pokemonId
+    })
+    pokemon.splice(7, 1)
+
+    res.sendStatus(204)
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
